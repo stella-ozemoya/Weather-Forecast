1c9335641b356308ee5c05cc60dfadf2a3043257
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-04-29 19:35:34 +0100

    Add files via upload
    search engine with API

--- forecast.js
+++ forecast.js
@@ -1,12 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature")
+    temperatureElement.innerHTML = response.data.temperature.current;
+    
+}
+
+function searchCity(city) {
+    let apiKey = "4052b3o3a5234d364a0003de67t9f22b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+    
+}
+
+
 function search(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-input");
     let cityElement = document.querySelector("#current-city");
     cityElement.innerHTML = searchInputElement.value
-   
+   searchCity(searchInputElement.value);
 }
 
 
 let serachForm = document.querySelector("#search-form");
 serachForm.addEventListener("submit", search);
+
+
 
